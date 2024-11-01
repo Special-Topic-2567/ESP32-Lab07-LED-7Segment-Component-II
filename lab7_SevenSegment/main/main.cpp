c649a8a118b72861fedf266c7e68b0b9b32735c4
65030034 <144875017+65030034@users.noreply.github.com>
2024-11-02 06:38:30 +0700

    Add files via upload

---
+++ lab7_SevenSegment/main/main.cpp
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "SevenSegment.h"
+
+SevenSegment s1(0);
+SevenSegment s2(4);
+
+extern "C" void app_main(void) {
+    while(1) {
+        s1.DisplayNumber(1);
+        s1.DisplayOn();
+        vTaskDelay(10/portTICK_PERIOD_MS);
+        s1.DisplayOff();
+
+        s2.DisplayNumber(5);
+        s2.DisplayOn();
+        vTaskDelay(10/portTICK_PERIOD_MS);
+        s2.DisplayOff();
+    }
+}
